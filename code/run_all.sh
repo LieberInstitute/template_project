44e939c13b1bd49f5ea7daf85ab5d0555f7f0062
Nick-Eagles <nick.eagles@libd.org>
2024-06-28 11:47:09 -0400

    Use SLURM with job dependency in the run_all shell script. Closes #2.

--- code/run_all.sh
+++ code/run_all.sh
@@ -1,52 +1,50 @@
 #!/bin/bash
-#$ -cwd
-#$ -l mem_free=2G,h_vmem=2G,h_fsize=100G
-#$ -N run_all_template_project
-#$ -o logs/run_all_template_project.txt
-#$ -e logs/run_all_template_project.txt
-#$ -m e
+#SBATCH -p bluejay
+#SBATCH --mem=10G
+#SBATCH --job-name=run_all_template_project
+#SBATCH -c 1
+#SBATCH -t 1-00:00:00
+#SBATCH -o logs/run_all_template_project.txt
+#SBATCH -e logs/run_all_template_project.txt
+#SBATCH --mail-type=ALL
 
-## Adapted from https://github.com/LieberInstitute/Visium_IF_AD/blob/master/code/run_all_post_spaceranger.sh
-## and https://github.com/LieberInstitute/ranger_metrics/blob/master/code/spaceranger/run_all.sh
+set -e
 
 echo "**** Job starts ****"
 date
 
 echo "**** JHPCE info ****"
 echo "User: ${USER}"
-echo "Job id: ${JOB_ID}"
-echo "Job name: ${JOB_NAME}"
-echo "Hostname: ${HOSTNAME}"
-echo "Task id: ${SGE_TASK_ID}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.3
 
 ## List current modules for reproducibility
 module list
 
-MAINDIR="/dcs04/lieber/lcolladotor/_jhpce_org_LIBD001/template_project"
+MAINDIR=$(git rev-parse --show-toplevel)
 CODEDIR="${MAINDIR}/code"
 PROCESSEDIR="${MAINDIR}/processed-data"
 
 ## Update code style
 cd ${CODEDIR}
-## Will crash if you don't have 'module load conda_R' in your ~/.bashrc file
-## See https://lcolladotor.github.io/bioc_team_ds/config-files.html#bashrc
 Rscript update_style.R
 
 ## Read in some raw-data into R
 cd ${CODEDIR}/01_read_data_to_r
 mkdir -p logs ## Create the logs directory if it doesn't exist
-# Delete outputs before re-generating them: log file + data
-rm logs/01_read_data_to_r.txt
-rm ${PROCESSEDIR}/01_read_data_to_r/penguins_data.csv
-qsub 01_read_data_to_r.sh
+# Delete output data before re-generating them:
+rm -f ${PROCESSEDIR}/01_read_data_to_r/penguins_data.csv
+id1=$(sbatch --parsable 01_read_data_to_r.sh)
 
 ## Explore the data
 cd ${CODEDIR}/02_explore_data
 mkdir -p logs ## Create the logs directory if it doesn't exist
-rm logs/01_ggpairs.txt
-qsub 01_ggpairs.sh
-rm logs/02_boxplots.txt
-qsub 02_boxplots.sh
+id2=$(sbatch --parsable --dependency=afterok:$id1 01_ggpairs.sh)
+sbatch --dependency=afterok:$id2 02_boxplots.sh
 
 
 ## Add future steps here
@@ -54,5 +52,5 @@ qsub 02_boxplots.sh
 echo "**** Job ends ****"
 date
 
-## This script was made using sgejobs version 0.99.1
-## available from http://research.libd.org/sgejobs/
+## This script was made using slurmjobs version 1.2.2
+## available from http://research.libd.org/slurmjobs/
